4ed030d638ad88ae37fb1643373242ca48fb5861
Valentino Wong <valentino.wong@gmail.com>
2019-06-28 04:42:37 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,15 +15,7 @@ def third_challenge
         :status => "alive"
         },
       :hero_friends => [ 
-        {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
-        }, {
-          :name => "Mercutio",
-          :age => "18",
-          :attitude => "hot-headed"
-        }
+        {:name => "Benvolio", :age => "17",:attitude => "worried"},{:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
       ]
    }, 
    :capulet => {
